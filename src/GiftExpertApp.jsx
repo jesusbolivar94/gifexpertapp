3ddacbb69328f61e79d6a08df54009bbc7d5e755
Jesus Bolivar <j5rojo94@gmail.com>
2022-10-15 22:31:23 -0300

    Valida que no se puedan agregar dos veces la misma categoria

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -7,6 +7,8 @@ export const GifExpertApp = () => {
 
     const onAddCategory = ( newCategory ) => {
 
+        if ( categories.includes( newCategory ) ) return
+
         setCategories( [ newCategory, ...categories ] )
 
     }

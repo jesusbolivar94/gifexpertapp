85948f44b5cc8a62372c48c4986a341d251884ff
Jesus Bolivar <j5rojo94@gmail.com>
2022-10-16 02:00:17 -0300

    Agrega custom hook useFetchGifs.js y muestra mensaje cargando condicional.

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,30 +1,23 @@
-import { useEffect, useState } from 'react'
-
-import { getGifs } from '../helpers/getGifs'
 import { GifItem } from './GifItem'
+import { useFetchGifs } from '../hooks/useFetchGifs'
 
 export const GifGrid = ( { category } ) => {
 
-    const [ images, setImages ] = useState([])
-
-    const getImages = async() => {
-        const newImages = await getGifs( category )
-        setImages( newImages )
-    }
-
-    useEffect( () => {
-
-        getImages()
-
-    }, [] )
+    const { images, isLoading } = useFetchGifs( category )
 
     return (
         <div className="mb-5">
-            <h2
-                className="font-heading text-xl font-medium pb-1 pr-3 border-b-2 border-stone-300"
-            >
-                { category }
-            </h2>
+            {
+                !isLoading && (
+                    <h2
+                        className="font-heading text-xl font-medium pb-1 pr-3 border-b-2 border-stone-300"
+                    >
+                        { `Resultados de: "${ category }"` }
+                    </h2>
+                )
+            }
+
+            { isLoading && ( <h3 className="text-xl text-stone-600 font-heading">Cargando...</h3> ) }
 
             <div className="flex flex-wrap items-start gap-x-10 gap-y-7 my-6">
                 { images.map( ( image ) =>

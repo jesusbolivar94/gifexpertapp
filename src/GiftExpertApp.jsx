25d50173bd05fa0ca2398155cfb1a4d86d7dc259
Jesus Bolivar <j5rojo94@gmail.com>
2022-10-15 21:05:27 -0300

    Agrega Tailwindcss

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -2,7 +2,7 @@
 export const GifExpertApp = () => {
     return (<>
         { /* Titulo */ }
-        <h1>GifExpertApp</h1>
+        <h1 className="text-2xl font-bold font-heading">GifExpertApp</h1>
 
         { /* Input */ }
 
